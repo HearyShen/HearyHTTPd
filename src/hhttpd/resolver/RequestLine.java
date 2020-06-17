4293101020ea3a2d9b6b5e70c6d55834d3353899
HearyShen <jiayun.shen@foxmail.com>
2020-06-17 16:35:32 +0800

    add GET query string resolve ability

--- src/hhttpd/resolver/RequestLine.java
+++ src/hhttpd/resolver/RequestLine.java
@@ -1,13 +1,24 @@
 package hhttpd.resolver;
 
+import java.util.Map;
+
 public class RequestLine {
     private String method;
     private String path;
+    private Map<String, String> querys;
     private String protocolAndVersion;
 
     public RequestLine(String method, String path, String protocolAndVersion) {
         this.method = method;
         this.path = path;
+        this.querys = null;
+        this.protocolAndVersion = protocolAndVersion;
+    }
+
+    public RequestLine(String method, String path, Map<String, String> querys, String protocolAndVersion) {
+        this.method = method;
+        this.path = path;
+        this.querys = querys;
         this.protocolAndVersion = protocolAndVersion;
     }
 
@@ -33,5 +44,13 @@ public class RequestLine {
 
     public void setProtocolAndVersion(String protocolAndVersion) {
         this.protocolAndVersion = protocolAndVersion;
+    }
+
+    public Map<String, String> getQuerys() {
+        return querys;
+    }
+
+    public void setQuerys(Map<String, String> querys) {
+        this.querys = querys;
     }
 }
--- src/hhttpd/resolver/RequestLineResolver.java
+++ src/hhttpd/resolver/RequestLineResolver.java
@@ -2,6 +2,8 @@ package hhttpd.resolver;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestLineResolver {
     public static RequestLine resolve(String requestLine) {
@@ -9,9 +11,39 @@ public class RequestLineResolver {
         if (firstLineSplits.length != 3) {
             throw new IllegalArgumentException("Invalid request line: '" + requestLine + "'");
         }
+
+        // resolve request method
         String requestMethod = firstLineSplits[0].strip();
-        String requestPath = URLDecoder.decode(firstLineSplits[1].strip(), StandardCharsets.UTF_8);
+
+        // resolve request URL
+        String requestURL = URLDecoder.decode(firstLineSplits[1].strip(), StandardCharsets.UTF_8);
+        String requestPath = requestURL;
+        Map<String, String> requestQueryKVs = null;
+        int queryStart = requestURL.indexOf("?");
+        if (queryStart >= 0) {
+            // has query string in GET
+            requestPath = requestURL.substring(0, queryStart);
+            String requestQueryString = requestURL.substring(queryStart+1);
+
+            if (! requestQueryString.isEmpty()) {
+                // resolve query key-value
+                requestQueryKVs = new HashMap<>();
+                for (String eachQueryString : requestQueryString.split("&")) {
+                    int equalSignIndex = eachQueryString.indexOf("=");
+                    String queryKey = eachQueryString;
+                    String queryValue = "";
+                    if (equalSignIndex >= 0) {
+                        queryKey = eachQueryString.substring(0, equalSignIndex);
+                        queryValue = eachQueryString.substring(equalSignIndex+1);
+                    }
+                    requestQueryKVs.put(queryKey, queryValue);
+                }
+            }
+        }
+
+        // resolve request protocol (and version)
         String requestProtocol = firstLineSplits[2].strip();
-        return new RequestLine(requestMethod, requestPath, requestProtocol);
+
+        return new RequestLine(requestMethod, requestPath, requestQueryKVs, requestProtocol);
     }
 }
